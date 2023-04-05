91960d53d254f75f5f146d9a82dbd67492cf8e99
Matthew Freeman <matthew.s.r.freeman@berkeley.edu>
2023-04-05 14:08:28 -0700

    Applied fixes from first on-bench test

--- take_distortion_obs.py
+++ take_distortion_obs.py
@@ -7,54 +7,57 @@ import subprocess
 import time
 import sys
 import ktl
-import epics
+#import epics
 import logging
 from datetime import datetime
 
  #--------- Dither Pattern -------------
 dither_spacing = 2.1 #mm
-dither_grid_size = 1 # e.g. 5 = 5x5 grid
+dither_grid_size = 2  # e.g. 5 = 5x5 grid
 position_angles =[0] # e.g. [0, 45] degrees
 pinhole_x = 90  #x_stage location for the centre of the dither pattern
-pinhole_y = 180
+pinhole_y = 185
 #--------------------------------------
 
 #-------------Keck Keywords--------------
-keck_kw = { 'ao':'ao1'
+keck_kw = { 'ao':'ao1',
 			'm1':'PCUM1POS',      #The same as x,y,z?
 			'm2':'PCUM2POS',
 			'm3':'PCUM3POS',
-			'r':'PCUPR',
-			'x':'PCUX',           #PCSFX
-			'y':'PCUY',           #PCSFY
-			'z':'PCUZ',           #PCSFLZ
-			'state':'PCUSTATE'    #PCSFSTATE    #old? 
-			'status':'PCUSTST'
-			'r_status':'PCURSTST'  #may show the same as status? Check if needed when doing rotational blocking moves.
-			'named_pos':'PCUNAME'  #PCSFNAME
-			'pinhole':'to_pinhole_mask',		
+			#'r':'PCUPR',
+			'x':'PCSFX',           #PCSFX
+			'y':'PCSFY',           #PCSFY
+			'z':'PCSFUZ',           #PCSFLZ
+			'state':'PCSFSTATE',    #PCSFSTATE    #old? 
+			'status':'PCSFSTST',
+			#'r_status':'PCURSTST',  #may show the same as status? Check if needed when doing rotational blocking moves.
+			'named_pos':'PCSFNAME',  #PCSFNAME
+			'pinhole':'pinhole_mask',		
 			}
 #----------------------------------------
 
 #---------epics keywords---------------
-pcu_status = epics.PV('k1:ao:pcu:stst')
-pcu_request = epics.PV('k1:ao:pcu:request')
-pcu_x = {'name':'x','write':epics.PV('k1:ao:pcu:M1Pos'),'read':epics.PV('k1:ao:pcu:M1Pos')}
-pcu_y = {'name':'y','write':epics.PV('k1:ao:pcu:M2Pos'),'read':epics.PV('k1:ao:pcu:M2Pos')}
-pcu_uz = {'name':'uz','write':epics.PV('k1:ao:pcu:M3Pos'),'read':epics.PV('k1:ao:pcu:M3Pos')}
-pcu_r = {'name':'r','write':epics.PV('k1:ao:pcu:rot:pos'),'read':epics.PV('k1:ao:pcu:rot:pos Rb')}
+#pcu_status = epics.PV('k1:ao:pcu:stst')
+#pcu_request = epics.PV('k1:ao:pcu:request')
+#pcu_x = {'name':'x','write':epics.PV('k1:ao:pcu:M1Pos'),'read':epics.PV('k1:ao:pcu:M1Pos')}
+#pcu_y = {'name':'y','write':epics.PV('k1:ao:pcu:M2Pos'),'read':epics.PV('k1:ao:pcu:M2Pos')}
+#pcu_uz = {'name':'uz','write':epics.PV('k1:ao:pcu:M3Pos'),'read':epics.PV('k1:ao:pcu:M3Pos')}
+#pcu_r = {'name':'r','write':epics.PV('k1:ao:pcu:rot:pos'),'read':epics.PV('k1:ao:pcu:rot:pos Rb')}
 #--------------------------------------
 
 def main(mode = 'keck_keywords'):
 
 	extent = (dither_grid_size-1) * dither_spacing /2
-	grid_steps_x = np.linspace(pinhole_x-extent,pinhole_y+extent,dither_grid_size)
+	grid_steps_x = np.linspace(pinhole_x-extent,pinhole_x+extent,dither_grid_size)
 	grid_steps_y = np.linspace(pinhole_y-extent,pinhole_y+extent,dither_grid_size)
+	
+	print('X grid steps = {}'.format(grid_steps_x))
+	print('Y grid steps = {}'.format(grid_steps_y))
 
-	subprocess.run(['lamp', 'dome', 1])  
-	subprocess.run(['iitime', 20])  	#in test 1, an integration time of 20 seconds gave peaks of ~7000 counts. Target 15,000 counts. Try integration time 40?
-	subprocess.run(['icoadds', 1])  
-	subprocess.run(['insamp', 4]) 		#number of reads
+	subprocess.run(['lamp', 'dome', '0'])  
+	subprocess.run(['iitime', '60'])  	#in test 1, an integration time of 20 seconds gave peaks of ~7000 counts. Target 15,000 counts. Try integration time 40?
+	subprocess.run(['icoadds', '1'])  
+	#subprocess.run(['insamp', '4']) 		#readout mode
 	print('(Set K rotator to 225?)')
 
 	# subprocess.run(['modify','-s','ao','obtdname=' + 'open'])   #Set TRICK to open or hband, takes 60 seconds.
@@ -81,16 +84,16 @@ def main(mode = 'keck_keywords'):
 
 	else:
 		blockMoveNP(keck_kw['pinhole'])
-		blockMove(keck_kw['m3'],99.32)
+		blockMove(keck_kw['z'],99.32)
 		for angle in position_angles:
-			blockMove(keck_kw['r'],65.7 + angle)
+			#blockMove(keck_kw['r'],65.7 + angle)
 			direction = 1
 			for y in grid_steps_y:
-				blockMove(keck_kw['m2'],y)	
+				blockMove(keck_kw['y'],y)	
 				for x in grid_steps_x[::direction]:
-					blockMove(keck_kw['m1'],x)	
+					blockMove(keck_kw['x'],x)	
 					img_filename = take_image()
-					log_entry(img_filename, pcu_x['read'].get(),pcu_y['read'].get(),pcu_uz['read'].get(),pcu_r['read'].get(),'')
+					#log_entry(img_filename, pcu_x['read'].get(),pcu_y['read'].get(),pcu_uz['read'].get(),pcu_r['read'].get(),'')
 				direction*=-1
 
 	print('Finished taking images')
@@ -123,8 +126,9 @@ def blockMoveNPEpics(target):
 		time.sleep(2)
 
 def blockMove(keyword, position):
+	print('Moving {} to {}'.format(keyword, position))
 	ktl.write(keck_kw['ao'], keyword, position)
-	time.sleep(1)
+	time.sleep(2)
 	while True:
 		pcu = ktl.read(keck_kw['ao'], keyword)
 		status = ktl.read(keck_kw['ao'], keck_kw['status'])
@@ -134,11 +138,12 @@ def blockMove(keyword, position):
 			return
 		elif status == 'FAULT':
 			sys.exit('Stage has FAULTED, exiting program..')
-			time.sleep(2)
+		time.sleep(2)
 
 def blockMoveNP(target):
+	print('Moving to named position {}'.format(target))
 	ktl.write(keck_kw['ao'], keck_kw['named_pos'], target)
-	time.sleep(1)
+	time.sleep(2)
 	while True:
 		pcux = ktl.read(keck_kw['ao'], keck_kw['x'])
 		pcuy = ktl.read(keck_kw['ao'], keck_kw['y'])
@@ -153,7 +158,7 @@ def blockMoveNP(target):
 		time.sleep(2)
 
 def take_image(n=1):
-	subprocess.run(['igoi',n],check='True')
+	subprocess.run(['igoi',str(n)],check='True')
 	filename = ktl.read('oids','lastfile')
 	# filename = subprocess.run(['lastimage'],capture_output=True,text=True)
 	return filename
@@ -170,5 +175,4 @@ def log_entry(filename,x,y,z,r,flag):
 main('keck_keywords')
 sys.exit(0)
 #--------------------------------
-
 
