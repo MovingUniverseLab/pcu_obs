2f8add301231912322fc39171db7a066cea4e88d
Matthew Freeman <matthew.s.r.freeman@berkeley.edu>
2023-04-05 16:11:20 -0700

    Split epics mode out into a separate funciton

--- take_distortion_obs.py
+++ take_distortion_obs.py
@@ -21,9 +21,9 @@ pinhole_y = 185
 
 #-------------Keck Keywords--------------
 keck_kw = { 'ao':'ao1',
-			'm1':'PCUM1POS',      #The same as x,y,z?
-			'm2':'PCUM2POS',
-			'm3':'PCUM3POS',
+			#'m1':'PCUM1POS',      #Duplicates of x,y,z?
+			#'m2':'PCUM2POS',
+			#'m3':'PCUM3POS',
 			#'r':'PCUPR',
 			'x':'PCSFX',           #PCSFX
 			'y':'PCSFY',           #PCSFY
@@ -45,59 +45,122 @@ keck_kw = { 'ao':'ao1',
 #pcu_r = {'name':'r','write':epics.PV('k1:ao:pcu:rot:pos'),'read':epics.PV('k1:ao:pcu:rot:pos Rb')}
 #--------------------------------------
 
-def main(mode = 'keck_keywords'):
-
+def main():
 	extent = (dither_grid_size-1) * dither_spacing /2
 	grid_steps_x = np.linspace(pinhole_x-extent,pinhole_x+extent,dither_grid_size)
 	grid_steps_y = np.linspace(pinhole_y-extent,pinhole_y+extent,dither_grid_size)
-	
 	print('X grid steps = {}'.format(grid_steps_x))
 	print('Y grid steps = {}'.format(grid_steps_y))
-
 	subprocess.run(['lamp', 'dome', '0'])  
 	subprocess.run(['iitime', '60'])  	#in test 1, an integration time of 20 seconds gave peaks of ~7000 counts. Target 15,000 counts. Try integration time 40?
 	subprocess.run(['icoadds', '1'])  
 	#subprocess.run(['insamp', '4']) 		#readout mode
 	print('(Set K rotator to 225?)')
-
 	# subprocess.run(['modify','-s','ao','obtdname=' + 'open'])   #Set TRICK to open or hband, takes 60 seconds.
 	# time.sleep(60) 
 	# subprocess.run(['ifilt', 'kp', 'open'])     #Set filter. eg 'Drk' or 'kp open' 
 	# time.sleep(10)
-
 	make_log()
 	log_entry('Filename','x','y','z','r','type')
 
-	if mode == 'epics':
-		blockMoveNPEpics('to_pinhole_mask')
-		blockMoveEpics(pcu_uz,99.32)
-		for angle in position_angles:
-			blockMoveEpics(pcu_r,65.7+angle)
-			direction = 1
-			for y in grid_steps_y:
-				blockMoveEpics(pcu_y,y)	
-				for x in grid_steps_x[::direction]:
-					blockMoveEpics(pcu_x,x)	
-					img_filename=take_image()
-					log_entry(img_filename, pcu_x['read'].get(),pcu_y['read'].get(),pcu_uz['read'].get(),pcu_r['read'].get(),'')
-				direction*=-1
-
-	else:
-		blockMoveNP(keck_kw['pinhole'])
-		blockMove(keck_kw['z'],99.32)
-		for angle in position_angles:
-			#blockMove(keck_kw['r'],65.7 + angle)
-			direction = 1
-			for y in grid_steps_y:
-				blockMove(keck_kw['y'],y)	
-				for x in grid_steps_x[::direction]:
-					blockMove(keck_kw['x'],x)	
-					img_filename = take_image()
-					#log_entry(img_filename, pcu_x['read'].get(),pcu_y['read'].get(),pcu_uz['read'].get(),pcu_r['read'].get(),'')
-				direction*=-1
-
+	#-----------starting moves-----------------
+	blockMoveNP(keck_kw['pinhole'])
+	blockMove(keck_kw['z'],99.32)
+	for angle in position_angles:
+		#blockMove(keck_kw['r'],65.7 + angle)
+		direction = 1
+		for y in grid_steps_y:
+			blockMove(keck_kw['y'],y)	
+			for x in grid_steps_x[::direction]:
+				blockMove(keck_kw['x'],x)	
+				img_filename = take_image()
+				
+				pcux = ktl.read(keck_kw['ao'], keck_kw['x'])
+				pcuy = ktl.read(keck_kw['ao'], keck_kw['y'])
+				pcupz = ktl.read(keck_kw['ao'], keck_kw['z'])
+				pcur = ktl.read(keck_kw['ao'], keck_kw['r'])
+				log_entry(img_filename,pcux,pcuy,pcupz,pcur)
+			direction*=-1
 	print('Finished taking images')
   
+
+def blockMove(keyword, position):
+	print('Moving {} to {}'.format(keyword, position))
+	ktl.write(keck_kw['ao'], keyword, position)
+	time.sleep(2)
+	while True:
+		pcu = ktl.read(keck_kw['ao'], keyword)
+		status = ktl.read(keck_kw['ao'], keck_kw['status'])
+		print(keyword +' is at ' + str(pcu) + ' and PCU state is ' + status)
+		if status == 'INPOS':
+			print(keyword +' has reached designated position')
+			return
+		elif status == 'FAULT':
+			sys.exit('Stage has FAULTED, exiting program..')
+		time.sleep(2)
+
+def blockMoveNP(target):
+	print('Moving to named position {}'.format(target))
+	ktl.write(keck_kw['ao'], keck_kw['named_pos'], target)
+	time.sleep(2)
+	while True:
+		pcux = ktl.read(keck_kw['ao'], keck_kw['x'])
+		pcuy = ktl.read(keck_kw['ao'], keck_kw['y'])
+		pculz = ktl.read(keck_kw['ao'], keck_kw['z'])
+		status = ktl.read(keck_kw['ao'], keck_kw['status'])
+		print('PCUX = ' + pcux + ', PCUY = ' + pcuy + ', PCUZ = ' + pculz + ' and PCU state is ' + status)
+		if status == 'INPOS':
+			print('Stage has reached designated position')
+			return
+		elif status == 'FAULT':
+			sys.exit('Stage has FAULTED, exiting program..')
+		time.sleep(2)
+
+def take_image(n=1):
+	subprocess.run(['igoi',str(n)],check='True')
+	filename = ktl.read('oids','lastfile')
+	# filename = subprocess.run(['lastimage'],capture_output=True,text=True)
+	return filename
+
+def make_log():
+	log_filename = datetime.now().strftime('PCU_%Y%m%d_%H%M%S.log')
+	print('Log filename:', log_filename)
+	logging.basicConfig(filename=log_filename, level=logging.INFO,format='%(message)s')
+
+def log_entry(filename,x,y,z,r,flag):
+	logging.info('{:>10} {:>10} {:>10} {:>10} {:>10} {:>10}'.format(filename,x,y,z,r,flag))
+
+
+def main_epics():
+	extent = (dither_grid_size-1) * dither_spacing /2
+	grid_steps_x = np.linspace(pinhole_x-extent,pinhole_x+extent,dither_grid_size)
+	grid_steps_y = np.linspace(pinhole_y-extent,pinhole_y+extent,dither_grid_size)
+	print('X grid steps = {}'.format(grid_steps_x))
+	print('Y grid steps = {}'.format(grid_steps_y))
+	subprocess.run(['lamp', 'dome', '0'])  
+	subprocess.run(['iitime', '60'])  	#in test 1, an integration time of 20 seconds gave peaks of ~7000 counts. Target 15,000 counts. Try integration time 40?
+	subprocess.run(['icoadds', '1'])  
+	#subprocess.run(['insamp', '4']) 		#readout mode
+	print('(Set K rotator to 225?)')
+	# subprocess.run(['modify','-s','ao','obtdname=' + 'open'])   #Set TRICK to open or hband, takes 60 seconds.
+	# time.sleep(60) 
+	# subprocess.run(['ifilt', 'kp', 'open'])     #Set filter. eg 'Drk' or 'kp open' 
+	# time.sleep(10)
+	make_log()
+	log_entry('Filename','x','y','z','r','type')
+	blockMoveNPEpics('to_pinhole_mask')
+	blockMoveEpics(pcu_uz,99.32)
+	for angle in position_angles:
+		blockMoveEpics(pcu_r,65.7+angle)
+		direction = 1
+		for y in grid_steps_y:
+			blockMoveEpics(pcu_y,y)	
+			for x in grid_steps_x[::direction]:
+				blockMoveEpics(pcu_x,x)	
+				img_filename=take_image()
+				log_entry(img_filename, pcu_x['read'].get(),pcu_y['read'].get(),pcu_uz['read'].get(),pcu_r['read'].get(),'')
+			direction*=-1
+	print('Finished taking images')
 
 def blockMoveEpics(channel,position,):
 	channel['write'].put(position)
@@ -125,54 +188,8 @@ def blockMoveNPEpics(target):
 			sys.exit('Stage has Faulted, exiting program.')
 		time.sleep(2)
 
-def blockMove(keyword, position):
-	print('Moving {} to {}'.format(keyword, position))
-	ktl.write(keck_kw['ao'], keyword, position)
-	time.sleep(2)
-	while True:
-		pcu = ktl.read(keck_kw['ao'], keyword)
-		status = ktl.read(keck_kw['ao'], keck_kw['status'])
-		print(keyword +' is at ' + str(pcu) + ' mm and state is ' + status)
-		if status == 'INPOS':
-			print(keyword +' has reached designated position')
-			return
-		elif status == 'FAULT':
-			sys.exit('Stage has FAULTED, exiting program..')
-		time.sleep(2)
-
-def blockMoveNP(target):
-	print('Moving to named position {}'.format(target))
-	ktl.write(keck_kw['ao'], keck_kw['named_pos'], target)
-	time.sleep(2)
-	while True:
-		pcux = ktl.read(keck_kw['ao'], keck_kw['x'])
-		pcuy = ktl.read(keck_kw['ao'], keck_kw['y'])
-		pculz = ktl.read(keck_kw['ao'], keck_kw['z'])
-		status = ktl.read(keck_kw['ao'], keck_kw['status'])
-		print('PCUX = ' + pcux + ', PCUY = ' + pcuy + ', PCUZ = ' + pculz + ' and state is ' + status)
-		if status == 'INPOS':
-			print('Stage has reached designated position')
-			return
-		elif status == 'FAULT':
-			sys.exit('Stage has FAULTED, exiting program..')
-		time.sleep(2)
-
-def take_image(n=1):
-	subprocess.run(['igoi',str(n)],check='True')
-	filename = ktl.read('oids','lastfile')
-	# filename = subprocess.run(['lastimage'],capture_output=True,text=True)
-	return filename
-
-def make_log():
-	log_filename = datetime.now().strftime('PCU_%Y%m%d_%H%M%S.log')
-	print('Log filename:', log_filename)
-	logging.basicConfig(filename=log_filename, level=logging.INFO,format='%(message)s')
-
-def log_entry(filename,x,y,z,r,flag):
-	logging.info('{:>10} {:>10} {:>10} {:>10} {:>10} {:>10}'.format(filename,x,y,z,r,flag))
-
 #--------------------------------
-main('keck_keywords')
+main()
 sys.exit(0)
 #--------------------------------
 
